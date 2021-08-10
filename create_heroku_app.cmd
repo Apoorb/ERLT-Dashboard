0f9304ca4769b3c3eb249bf748c5d7a9717139ce
Apoorba Bibeka <apoorb2510@gmail.com>
2021-08-09 22:08:24 -0400

    Initial commit

---
+++ create_heroku_app.cmd
@@ -0,0 +1,15 @@
+REM login to your Heroku account
+heroku login
+
+REM change dash-app to a unique name
+heroku create multipage-erlt-dashboard-3
+REM change remote
+heroku git:remote -a multipage-erlt-dashboard-3
+git remote -v
+
+REM deploy to Heroku
+git push heroku master
+
+REM run the app with a 1 heroku "dyno"
+heroku ps:scale web=1
+
