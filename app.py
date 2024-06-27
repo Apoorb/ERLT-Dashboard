7bd41c09e5c7aca53f65845a8657794af643fc53
Apoorb <apoorb2510@gmail.com>
2024-06-27 12:58:01 -0500

    add flask app

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 import dash
 import dash_bootstrap_components as dbc
-
+from flask import Flask
 
 # bootstrap theme
 # https://bootswatch.com/darkly/
 external_stylesheets = [dbc.themes.DARKLY]
 
-
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+flask_server = Flask(__name__)
+app = dash.Dash(__name__, server=flask_server, external_stylesheets=external_stylesheets)
 
 
 server = app.server
