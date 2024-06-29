f8f254920322355d64e297f0ba287775c2de2eda
Apoorb <apoorb2510@gmail.com>
2024-06-29 18:28:57 -0500

    changes for render.com

--- my_app.py
+++ my_app.py
@@ -7,6 +7,5 @@ external_stylesheets = [dbc.themes.DARKLY]
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-
 server = app.server
 app.config.suppress_callback_exceptions = True
